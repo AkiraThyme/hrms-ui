8c32f7613fea9bd14cf546aa805a1839d508b203
DESKTOP-PRGAHPH\Jay <cb.jearranzado@outlook.com>
2025-06-13 15:07:23 +0800

    initial commit hrms ui

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import '@/assets/styles/index.scss'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const pinia = createPinia()
+
+createApp(App)
+  .use(pinia)
+  .use(router)
+  .mount('#app')
---
+++ vite.config.js
@@ -0,0 +1,52 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+//libs
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+
+// https://vite.dev/config/
+export default defineConfig({
+  clearScreen: true,
+  plugins: [
+    vue(),
+    Icons({
+      autoInstall: true,
+    }),
+    Components({
+      dirs: ['src/components'],
+      dts: 'components.d.ts',
+      resolvers: [
+        IconsResolver({
+          prefix: 'icon',
+        }),
+      ],
+    }),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'pinia',
+        'vue/macros',
+        '@vueuse/head',
+        '@vueuse/core',
+      ],
+      dts: 'auto-imports.d.ts',
+      dirs: ['src/composables', 'src/stores'],
+      vueTemplate: true,
+
+      eslintrc: {
+        enabled: true
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
